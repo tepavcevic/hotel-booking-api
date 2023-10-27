3e6e8a52eeefe9a63239bacbcfd1ef5160649857
tepavcevic <djordje.tepa@gmail.com>
2023-10-27 13:44:20 +0200

    Update seed script to create bookings as well

--- scripts/seed.go
+++ scripts/seed.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/tepavcevic/hotel-reservation/db"
 	"github.com/tepavcevic/hotel-reservation/types"
@@ -12,20 +13,22 @@ import (
 )
 
 const (
-	hotelCollName = "hotels"
-	roomCollName  = "rooms"
-	userCollName  = "users"
+	hotelCollName   = "hotels"
+	roomCollName    = "rooms"
+	userCollName    = "users"
+	bookingCollName = "bookings"
 )
 
 var (
-	client     *mongo.Client
-	roomStore  db.RoomStore
-	hotelStore db.HotelStore
-	userStore  db.UserStore
-	ctx        = context.Background()
+	client       *mongo.Client
+	roomStore    db.RoomStore
+	hotelStore   db.HotelStore
+	userStore    db.UserStore
+	bookingStore db.BookingStore
+	ctx          = context.Background()
 )
 
-func seedUser(isAdmin bool, email, firstName, lastName, password string) {
+func seedUser(isAdmin bool, email, firstName, lastName, password string) *types.User {
 	user, err := types.NewUserFromParams(types.CreateUserParams{
 		FirstName: firstName,
 		LastName:  lastName,
@@ -36,56 +39,64 @@ func seedUser(isAdmin bool, email, firstName, lastName, password string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, err = userStore.CreateUser(ctx, user)
+	dbUser, err := userStore.CreateUser(ctx, user)
 	if err != nil {
 		log.Fatal(err)
 	}
+	return dbUser
 }
 
-func seedHotel(name, location string, rating int) {
+func seedHotel(name, location string, rating int) *types.Hotel {
 	hotel := types.Hotel{
 		Name:     name,
 		Location: location,
 		Rooms:    []primitive.ObjectID{},
 		Rating:   rating,
 	}
-	rooms := []types.Room{
-		{
-			Size:    types.SmallRoom,
-			Seaside: false,
-			Price:   65,
-		},
-		{
-			Size:    types.NormalRoom,
-			Seaside: true,
-			Price:   99,
-		},
-		{
-			Size:    types.KingSizeRoom,
-			Seaside: true,
-			Price:   189,
-		},
-	}
 	dbHotel, err := hotelStore.Create(ctx, &hotel)
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, room := range rooms {
-		room.HotelID = dbHotel.ID
-		dbRoom, err := roomStore.CreateRoom(ctx, &room)
-		if err != nil {
-			log.Fatal(err)
-		}
-		dbHotel.Rooms = append(dbHotel.Rooms, dbRoom.ID)
+	return dbHotel
+}
+
+func seedRoom(size types.Size, seaside bool, price float64, hotelID primitive.ObjectID) *types.Room {
+	room := types.Room{
+		HotelID: hotelID,
+		Size:    size,
+		Seaside: seaside,
+		Price:   price,
+	}
+	dbRoom, err := roomStore.CreateRoom(ctx, &room)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return dbRoom
+}
+
+func seedBooking(roomID, userID primitive.ObjectID, fromDate, tillDate time.Time, numPersons int, cancelled bool) {
+	booking := types.Booking{
+		RoomID:     roomID,
+		UserID:     userID,
+		FromDate:   fromDate,
+		TillDate:   tillDate,
+		NumPersons: numPersons,
+		Cancelled:  cancelled,
+	}
+	_, err := bookingStore.CreateBooking(ctx, &booking)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
 func main() {
 	seedHotel("Transilvania", "Romania", 8)
 	seedHotel("Moskva", "Serbia", 7)
-	seedHotel("Swissotel", "Bosnia", 8)
-	seedUser(false, "son@momoaa.com", "Momo", "Jsoon", "greatestpasswordever")
+	swissotel := seedHotel("Swissotel", "Bosnia", 8)
+	json := seedUser(false, "son@momoaa.com", "Momo", "Jsoon", "greatestpasswordever")
 	seedUser(true, "mario@dreznjak.com", "Mario", "Dreznjak", "admin123")
+	room := seedRoom(types.NormalRoom, false, 87.99, swissotel.ID)
+	seedBooking(room.ID, json.ID, time.Now(), time.Now().AddDate(0, 0, 4), 3, false)
 }
 
 func init() {
@@ -97,6 +108,7 @@ func init() {
 	hotelStore = db.NewMongoHotelStore(client, db.DBNAME, hotelCollName)
 	roomStore = db.NewMongoRoomStore(client, db.DBNAME, roomCollName, hotelStore)
 	userStore = db.NewMongoUserStore(client, db.DBNAME, userCollName)
+	bookingStore = db.NewMongoBookingStore(client, db.DBNAME, bookingCollName)
 	if err := client.Database(db.DBNAME).Collection(hotelCollName).Drop(ctx); err != nil {
 		log.Fatal(err)
 	}
@@ -104,6 +116,9 @@ func init() {
 		log.Fatal(err)
 	}
 	if err := client.Database(db.DBNAME).Collection(userCollName).Drop(ctx); err != nil {
+		log.Fatal(err)
+	}
+	if err := client.Database(db.DBNAME).Collection(bookingCollName).Drop(ctx); err != nil {
 		log.Fatal(err)
 	}
 }
